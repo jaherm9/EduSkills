80a817ee0f57ce7b4f255e61e2be02b975732bd6
Minhaz <minhajulabedin404@gmail.com>
2021-10-04 22:06:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+import Home from './Component/Home/Home';
+import About from './Component/About/About';
+import Service from './Component/Service/Service';
+import Navigation from './Component/Navigation/Navigation';
+import Footer from './Component/Footer/Footer';
+import Header from './Component/Header/Header';
+import NotFound from './Component/NotFound/NotFound';
+
+import Training from './Component/Training/Training'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+  <BrowserRouter>
+<Navigation></Navigation>
+
+
+
+  <Switch>
+    <Route exact path="/">
+      <Home></Home>
+    </Route>
+
+    <Route  path="/home">
+      <Home></Home>
+    </Route>
+
+    <Route path="/service">
+      <Service></Service>
+    </Route>
+
+    <Route path="/training">
+      <Training></Training>
+    </Route>
+
+    <Route path="/about">
+    <About></About>
+      </Route>
+
+    <Route path="*">
+      <NotFound></NotFound>
+    </Route>
+  
+
+  </Switch>
+
+  
+  
+  </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+<script src="https://kit.fontawesome.com/babda89756.js" crossorigin="anonymous"></script>
+
 
 ReactDOM.render(
   <React.StrictMode>
